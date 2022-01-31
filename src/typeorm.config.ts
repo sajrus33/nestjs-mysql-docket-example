fbda4926f10563a050d83a959703f54c318198e6
Brian Wala <brian.wala@kevin.eu>
2022-01-31 10:39:58 +0100

    prepare: using env

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Users } from './users.entity';
 
@@ -5,9 +6,9 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: 'mydb',
   port: 3306,
-  username: 'root',
-  password: 'DE-k&ySJ6a5BrY9G',
-  database: 'localmysql',
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   entities: [Users],
   synchronize: true,
   // migrations: ['dist/src/db/migrations/*.js'],
